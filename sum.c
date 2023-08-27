99e6629cfcf27f06562ce479d4acf04de19c0e8c
SrijanJha17 <141908898+SrijanJha17@users.noreply.github.com>
2023-08-27 20:10:02 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+void main() 
+{
+    int x;
+    int y;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    printf("Enter another number: ");
+    scanf("%d", &y);
+    printf("Sum of numbers: %d ",x+y);
+    return 0;
+}
